a8de52b634260173e2bb8be37816131b80e86885
Artemmm94 <100225936+Artemmm94@users.noreply.github.com>
2022-10-05 21:27:22 +0300

    Add files via upload

---
+++ INDEX.js
@@ -0,0 +1,65 @@
+alert(1);
+const todosNote = document.querySelector(`.js-todos`);
+const inputNode = document.querySelector(`.js-input`);
+const btnNode = document.querySelector(`.js-btn`);
+let todos = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    done: false,
+    id: `${Math.random()}`
+  };
+
+  todos.push(todo);
+}
+
+function deleteTodo(id) {
+    todos.forEach(todo => {
+        if (todo.id === id) {
+            todo.done = true;
+        
+        }
+    })
+}
+
+function render() {
+    console.log(todos);
+     let html = '';
+
+    todos.forEach(todo => {
+      if (todo.done) {
+        return;
+      };
+
+      html += `
+           <div>
+           ${todo.text}
+           <button data-id='${todo.id}'>Сделано</button>
+           </div>
+      `;
+    })
+
+    todosNode.innerHTML = html; 
+}
+
+btnNode.addEventListener(`click`, () => {
+  const text = inputNode.nodeValue;
+
+  addTodo(text);
+
+  render();
+});
+
+todosNode.addEventListener(`click`, (event) => {
+  if (event.target.tagName !== `BUTTON`) {
+    return;
+  }
+const id = event.target.dataset.id;
+
+deleteTodo(id);
+
+render();
+})
+
+render();
